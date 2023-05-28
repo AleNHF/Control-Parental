3daafd5916e54bc58ca0ac483ad29246e4f919af
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2023-05-28 00:51:46 -0400

    probando Rekognition

--- QstodioWeb/app/Http/Controllers/UploadImageController.php
+++ QstodioWeb/app/Http/Controllers/UploadImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Image;
+use Aws\Rekognition\RekognitionClient;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -15,8 +16,8 @@ class UploadImageController extends Controller
      */
     public function index()
     {
-        $imagenes=Image::all();
-        return view('uploadImage',compact('imagenes'));
+        $imagenes = Image::all();
+        return view('uploadImage', compact('imagenes'));
     }
 
     /**
@@ -50,13 +51,53 @@ class UploadImageController extends Controller
             $image = Image::create([
                 'url' => $url
             ]);
-            return $url;
+            //  return $url;
             // ProcessVerificarFoto::dispatch($image->id);
         }
-        return "no es imagen";
-        // return redirect('/');
+        //return "no es imagen";
+        return redirect('/');
     }
+    public function controlImagen(Request $request)
+    {
 
+
+
+        if ($request->hasFile('foto')) {
+
+            $client = new RekognitionClient([
+                'version' => 'latest',
+                'region' => env('AWS_DEFAULT_REGION'),
+                'credentials' => [
+                    'key' => env('AWS_ACCESS_KEY_ID'),
+                    'secret' => env('AWS_SECRET_ACCESS_KEY'),
+                ],
+            ]);
+
+            /* OBTENIENDO LA IMG */
+            $image = fopen($request->file('foto')->getPathName(), 'r');
+            $bytes = fread($image, $request->file('foto')->getSize());
+
+
+            /* CONSULTANDO EL SERVICIO DE AWS */
+
+            $result = $client->detectModerationLabels([
+                'Image' => ['Bytes' => $bytes],
+                'MinConfidence' => 51
+
+            ]);
+            $labels = $result['ModerationLabels'] ?? []; //verifica si retorna etiquetas de descripción de la foto de contenido inapropiado, si la foto no tuviera contenido inapropiado no devolvería nada
+
+            if ($labels != []) {
+                $carpeta = "Contenido"; //Carpeta en la que se guardará en S3
+
+                $url = Storage::disk('s3')->put($carpeta, $request->foto, 'public'); //Subimos la imagen a S3, retorna la url de S3
+                $image = Image::create([ //Creamos la imagen, podemos extraer el contenido de array labels, para ver que contenido inapropiado tiene la imagen
+                    'url' => $url
+                ]);
+            }
+            return response()->json(['labels' => $labels]);
+        }
+    }
     /**
      * Display the specified resource.
      *
